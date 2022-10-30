69fddd4cda449e5595ddfc1fd0f7383c4946ff44
rasikhmhd <rasikhmoha@gmail.com>
2022-10-30 16:05:45 +0400

    Second Commit

---
+++ Assets/Scripts/ItemCollector.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Text = TMPro.TextMeshProUGUI;
+
+public class ItemCollector : MonoBehaviour
+{
+    private int strawberries = 0;
+    [SerializeField] private Text strawberriesText;
+    [SerializeField] private AudioSource collectionSoundEffect;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Strawberry"))
+        {
+            collectionSoundEffect.Play();
+            Destroy(collision.gameObject);
+            strawberries++;
+            strawberriesText.text = "Strawberries: " + strawberries;
+        }
+    }
+}
